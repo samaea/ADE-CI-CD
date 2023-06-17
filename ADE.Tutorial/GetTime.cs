9f8e66217de0d598b6bd5de00c2a7aa3446649de
samaea <samaea-gh@outlook.com>
2023-06-18 00:20:33 +0100

    Verbatim change.

--- ADE.Tutorial/GetTime.cs
+++ ADE.Tutorial/GetTime.cs
@@ -34,7 +34,7 @@ namespace ADE.Tutorial
 
             var now = DateTime.UtcNow;
 
-            response.WriteString($"UTC Time: {now}");
+            response.WriteString($"The current UTC Time is: {now}");
 
             foreach (var timeZone in timeZones)
                 response.WriteString($"{(timeZone.IsDaylightSavingTime(now) ? timeZone.DaylightName : timeZone.StandardName)}: {TimeZoneInfo.ConvertTimeFromUtc(now, timeZone)}");
